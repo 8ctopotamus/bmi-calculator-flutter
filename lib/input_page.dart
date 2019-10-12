51fa1252273f8f8f7cfafcf1f1a1b855a4fc6738
8ctopotamus <joshnaylor88@gmail.com>
2019-10-12 17:35:06 -0500

    ReusableCard and IconContent

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -22,10 +24,20 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: <Widget>[
               Expanded(child: ReusableCard(
-                  colour: activeCardColor)
+                  colour: activeCardColor,
+                  cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
+                  ),
+                ),
               ),
               Expanded(child: ReusableCard(
-                  colour: activeCardColor)
+                  colour: activeCardColor,
+                  cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
+                  ),
+                )
               ),
             ]
           )),
@@ -51,3 +63,5 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
+
