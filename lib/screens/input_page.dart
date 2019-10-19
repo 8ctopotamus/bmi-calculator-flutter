2736e0e617fe8558606ae6ec34efe442c5a3d414
8ctopotamus <joshnaylor88@gmail.com>
2019-10-19 12:48:56 -0500

    BMI Calculation

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -6,6 +6,7 @@ import 'package:bmi_calculator/components/icon_content.dart';
 import 'package:bmi_calculator/constants.dart';
 import 'package:bmi_calculator/components/bottom_button.dart';
 import 'package:bmi_calculator/components/round_icon_button.dart';
+import 'package:bmi_calculator/calculator_brain.dart';
 
 enum Gender {
  MALE,
@@ -191,9 +192,17 @@ class _InputPageState extends State<InputPage> {
           BottomButton(
             text: 'CALCULATE',
             onTap: () {
+              CalculatorBrain calc = CalculatorBrain(
+                height: height,
+                weight: weight,
+              );
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => ResultsPage())
+                MaterialPageRoute(builder: (context) => ResultsPage(
+                  bmiResult: calc.calculateBMI(),
+                  resultText: calc.getResult(),
+                  interpretation: calc.getInterpretation(),
+                )),
               );
             },
           ),
